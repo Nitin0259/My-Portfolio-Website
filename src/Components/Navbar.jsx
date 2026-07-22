583199c011e7cae6d2f7cea3b6394c19826d500f
Nitin0259 <ns7034501@gmail.com>
2026-07-22 20:38:07 +0530

    Work on Home Section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -38,27 +38,23 @@ const Navbar = () => {
             <div className="absolute -inset-x-3 -inset-y-2 rounded-lg bg-[#82456c]/10 opacity-0 blur-md transition-opacity duration-300 group-hover:opacity-100"></div>
 
             <div className="relative flex items-center gap-0.5 sm:gap-1">
-              {/* Opening bracket */}
+
               <span className="text-[#82456c] transition-transform duration-300 group-hover:-translate-x-0.5">
                 &lt;
               </span>
 
-              {/* First Name */}
               <span className="text-white transition-colors duration-300 group-hover:text-gray-100">
                 Nitin
               </span>
 
-              {/* Slash */}
-              <span className="text-[#82456c] px-0.5 transition-transform duration-300 group-hover:scale-110">
+              <span className="text-[#82456c] px-0.1 transition-transform duration-300 group-hover:scale-110">
                 /
               </span>
 
-              {/* Last Name */}
               <span className="bg-linear-to-r from-white via-gray-200 to-gray-400 bg-clip-text text-transparent transition-all duration-300">
                 Singh
               </span>
 
-              {/* Closing bracket */}
               <span className="text-[#82456c] transition-transform duration-300 group-hover:translate-x-0.5">
                 &gt;
               </span>
@@ -87,10 +83,30 @@ const Navbar = () => {
                 </li>
               );
             })}
+
+            {/* Mobile Social Icons */}
+            <div className="mt-2 flex items-center gap-4 lg:hidden">
+              {/* GitHub */}
+              <a href="https://github.com/Nitin0259"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex h-10 w-10 items-center justify-center rounded-full border border-white/10 bg-white/5 text-slate-300 backdrop-blur-xl transition duration-300 hover:border-cyan-400 hover:bg-cyan-500/10 hover:text-cyan-400">
+                <i className="fa-brands fa-github"></i>
+              </a>
+
+              {/* Linkedin */}
+              <a href="https://www.linkedin.com/in/nitin-singh-a6524a39a/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex h-10 w-10 items-center justify-center rounded-full border border-white/10 bg-white/5 text-slate-300 backdrop-blur-xl transition duration-300 hover:border-cyan-400 hover:bg-cyan-500/10 hover:text-cyan-400">
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </div>
+
           </ul>
 
           {/* Right-part */}
-          <div className='hidden items-center gap-4 lg:flex'>
+          <div className='hidden lg:flex items-center gap-4'>
 
             {/* GitHub */}
             <a href="https://github.com/Nitin0259"
@@ -112,7 +128,7 @@ const Navbar = () => {
           {/* Mobile Hamburger */}
           <button
             onClick={() => setSidebarOpen(!sidebarOpen)}
-            className="flex lg:hidden h-10 w-10 items-center justify-center rounded-lg border border-white/10 bg-white/5 text-white"
+            className="flex lg:hidden h-9 w-9 items-center justify-center rounded-lg border border-white/10 bg-white/5 text-white"
           >
             <i className={`fa-solid ${sidebarOpen ? "fa-xmark" : "fa-bars"} text-xl`} />
           </button>
