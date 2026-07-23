9b22b94821153fdd457def774ed61b5073e6255f
Nitin0259 <ns7034501@gmail.com>
2026-07-23 13:20:34 +0530

    Complete home section

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ const App = () => {
   return (
     <div className='relative min-h-screen overflow-x-hidden bg-linear-to-br from-[#050816] via-[#0B1120] to-[#020617] text-white'>
       {/* <!-- Cyan Glow --> */}
-    <div class="absolute -top-40 -left-40 h-96 w-96 rounded-full bg-cyan-500/20 blur-[160px]"></div>
+    <div className="absolute -top-40 -left-40 h-96 w-96 rounded-full bg-cyan-500/20 blur-[160px]"></div>
 
     {/* <!-- Purple Glow --> */}
-    <div class="absolute bottom-0 right-0 h-125 w-125 rounded-full bg-purple-600/20 blur-[180px]"></div>
+    <div className="absolute bottom-0 right-0 h-125 w-125 rounded-full bg-purple-600/20 blur-[180px]"></div>
 
       {/* Content */}
       <div className='relative z-10'>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,58 +1,67 @@
-import React from 'react'
-import ReactTypingEffect from 'react-typing-effect'
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+import HeroImage from "../assets/Images/hero_image.png";
+import Resume from "../assets/Images/resume.pdf"
 
 const Home = () => {
+
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Web Developer",
+        "Full Stack Developer",
+        "Python Developer"
+      ],
+      typeSpeed: 80,
+      backSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <div className='pt-32 pb-12'>
-      <main className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 py-4'>
-        <div className='flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-8'>
+    <div className='min-h-screen flex items-center pt-28 pb-12'>
+      <main className='max-w-8xl mx-auto px-4 sm:px-6 lg:px-10 py-4'>
+        <div className='flex flex-col lg:flex-row items-center justify-between gap-16 lg:gap-10'>
 
           {/* Left-part */}
           <div className='flex-1 flex flex-col items-start text-left space-y-6 w-full'>
 
-            <div className="realtive inline-flex items-center gap-2 px-3 py-1.5 rounded-full bg-white/5 border border-white/10 backdrop-blur-md">
+            <div className="relative inline-flex items-center gap-2 px-3 py-1.5 rounded-full bg-white/5 border border-white/10 backdrop-blur-md">
               <span className="h-2 w-2 rounded-full bg-emerald-400 animate-pulse"></span>
               <span className="text-xs font-medium tracking-wide text-gray-300">Available for work</span>
             </div>
 
             {/* Heading */}
-            <div className='space-y-2'>
-              <p className='text-lg sm:text-xl text-gray-400 font-medium'>Hello, I'm</p>
-              <h1 className='text-4xl sm:text-5xl lg:text-6xl font-extrabold tracking-tight text-white'>Nitin <span className='bg-linear-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent'>Singh</span></h1>
-            </div>
+            <div className="space-y-4">
 
-            {/* subheading */}
-            <div className="flex flex-wrap items-center gap-2 text-base sm:text-lg font-medium text-gray-300">
-              <ReactTypingEffect
-              text={[
-                'Fullstack Developer',
-                'Web Developer',
-                'Python Developer',
-                'Backend Developer'
-              ]}
-              speed={100}
-              eraseSpeed={50}
-              typingDelay={500}
-              eraseDelay={2000}
-              cursorRenderer={(cursor) => {
-                return <span className='text-[#8245ec]'>{cursor}</span>
-              }}
-              >
-              </ReactTypingEffect>
-              {/* <div className="flex flex-wrap gap-x-3 gap-y-1 text-xs text-gray-400">
-                <span>Frontend Developer</span>
-                <span>•</span>
-                <span>Backend Developer</span>
-                <span>•</span>
-                <span>Python Developer</span>
-              </div> */}
+              <h1 className="text-2xl sm:text-3xl lg:text-5xl font-bold leading-tight text-white">
+                Hi, My name is{" "}
+                <span className="text-purple-400 ">
+                  Nitin
+                </span>
+                <br />
+
+                and I am a passionate
+                <br />
+
+                <div className="text-purple-400 ">
+                  <span ref={el}></span>
+                </div>
+              </h1>
+
             </div>
 
             {/* Description */}
-            <p className='text-gray-400 text-sm sm:text-base max-w-xl leading-relaxed'>I build exceptional digital experiences that are fast, accessible, visually appealing, and responsive.</p>
+            <p className='text-gray-400 text-md sm:text-base max-w-md leading-relaxed'>I build exceptional digital experiences that are fast, accessible, visually appealing, and responsive.</p>
 
             <div className='flex flex-wrap items-center gap-4 pt-2'>
-              <a href="" className='px-6.5 py-3 rounded-xl font-semibold text-sm bg-linear-to-r from-cyan-500 to-purple-600 text-white shadow-cyan-500/25 hover:shadow-cyan-500/40 hover:scale-[1.02] transition-all duration-300 text-center'>Download Resume</a>
+              <a href={Resume} target="_blank" className='px-6.5 py-3 rounded-xl font-semibold text-sm bg-linear-to-r from-cyan-500 to-purple-600 text-white shadow-cyan-500/25 hover:shadow-cyan-500/40 hover:scale-[1.02] transition-all duration-300 text-center'>Download Resume</a>
 
               <a href="" className='px-6.5 py-3 rounded-xl font-semibold text-sm bg-white/5 border border-white/10 text-white hover:bg-white/10 hover:border-white/20 transition-all duration-300 text-center backdrop-blur-md'>View Projects</a>
             </div>
@@ -65,8 +74,8 @@ const Home = () => {
             <div className="absolute w-72 h-72 sm:w-96 sm:h-96 bg-purple-500/10 rounded-full blur-3xl -z-10 pointer-events-none translate-x-20"></div>
 
             <img
-              src="./assets/Images/hero_image.webp" alt="Developer Workspace Illustration"
-              className="w-full max-w-lg lg:max-w-xl h-auto object-contain drop-shadow-2xl"
+              src={HeroImage} alt="Developer Workspace Illustration"
+              className="w-full max-w-lg lg:max-w-xl object-contain drop-shadow-2xl"
             />
           </div>
 
